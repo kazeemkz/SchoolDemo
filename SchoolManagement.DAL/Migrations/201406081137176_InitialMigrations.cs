cec2cd2aa1d1b158198d506b1a057d8491182e09
kazeem oyebode <kazeemkz@yahoo.com>
2016-04-22 18:06:06 +0200

    update2

--- SchoolManagement.DAL/Migrations/201406081137176_InitialMigrations.cs
+++ SchoolManagement.DAL/Migrations/201406081137176_InitialMigrations.cs
@@ -1,16 +1,16 @@
-namespace eLibrary.DAL
-{
-    using System;
-    using System.Data.Entity.Migrations;
+//namespace eLibrary.DAL
+//{
+//    using System;
+//    using System.Data.Entity.Migrations;
     
-    public partial class InitialMigrations : DbMigration
-    {
-        public override void Up()
-        {
-        }
+//    public partial class InitialMigrations : DbMigration
+//    {
+//        public override void Up()
+//        {
+//        }
         
-        public override void Down()
-        {
-        }
-    }
-}
+//        public override void Down()
+//        {
+//        }
+//    }
+//}
